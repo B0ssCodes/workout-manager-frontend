a5f63fddf025ddb5b89363a6d7d7029c22ad9976
Boss Codes <jadsaderkarate@gmail.com>
2024-01-05 20:48:56 +0200

    potentialFrontendFix

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://workout-manager-inc0.onrender.com',
+  baseURL: 'https://workout-manager-inc0.onrender.com:10000',
 });
 
 export default api;
