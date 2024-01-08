4b887995fd7867a0110d8f48ad5ad048991f772a
Boss Codes <jadsaderkarate@gmail.com>
2024-01-08 21:35:44 +0200

    Password Visibility Added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useLogin } from "../hooks/useLogin";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 
 const Login = () => {
@@ -8,6 +9,12 @@ const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const {login, error, isLoading} = useLogin()
+    const [passwordVisible, setPasswordVisible] = useState(false)
+
+
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(prevPasswordVisible => !prevPasswordVisible);
+      };
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -20,18 +27,32 @@ const Login = () => {
             <h3>Log In</h3>
 
             <label>Email:</label>
+            <div className="password-input">
             <input 
             type="email"
             onChange={(e) => setEmail(e.target.value)}
             value={email}
             />
+            <motion.span className="material-symbols-outlined hidden"
+            onClick= {togglePasswordVisibility}
+            whileHover={{scale: 1.1}}>
+            {passwordVisible ? "visibility" : "visibility_off"}
+            </motion.span>
+            </div>
 
             <label>Password:</label>
+            <div className="password-input">
             <input 
-            type="password"
+            type={passwordVisible ? "text" : "password"}
             onChange={(e) => setPassword(e.target.value)}
             value={password}
             />
+            <motion.span className="material-symbols-outlined"
+            onClick= {togglePasswordVisibility}
+            whileHover={{scale: 1.1}}>
+            {passwordVisible ? "visibility" : "visibility_off"}
+            </motion.span>
+            </div>
         <button disabled={isLoading}>Log In</button>
         <div id="signup-text">Don't have an account?</div>
         <Link id="signup-button" to="/signup">Sign Up</Link>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,18 +1,48 @@
 import React, { useState } from "react";
 import { useSignup } from "../hooks/useSignup";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Signup = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("")
+    const [passwordsMatch, setPasswordsMatch] = useState(true)
+    const [passwordVisible, setPasswordVisible] = useState(false)
+    const [submitAttempted, setSubmitAttempted] = useState(false);
+
     const {signup, error, isLoading} = useSignup()
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+   const handlePasswordChange = (e) => {
+         setPassword(e.target.value)
+         if(e.target.value !== passwordConfirm){
+              setPasswordsMatch(false)
+         } else {
+              setPasswordsMatch(true)
+         }
+        }
 
-       await signup(email, password)
-    }
+        const handlePasswordConfirmChange = (e) => {
+            setPasswordConfirm(e.target.value);
+            if (password !== e.target.value) {
+              setPasswordsMatch(false);
+            } else {
+              setPasswordsMatch(true);
+            }
+          };
+
+          const handleSubmit = async (e) => {
+            e.preventDefault();
+            setSubmitAttempted(true);
+            if (passwordsMatch) {
+              await signup(email, password);
+            }
+          };
+
+          const togglePasswordVisibility = () => {
+            setPasswordVisible(prevPasswordVisible => !prevPasswordVisible);
+          };
 
     return (
         <form className="signup" onSubmit={handleSubmit} >
@@ -26,16 +56,37 @@ const Signup = () => {
             />
 
             <label>Password:</label>
+            <div className="password-input">
             <input 
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
+            type={passwordVisible ? "text" : "password"}
+            onChange={handlePasswordChange}
             value={password}
             />
-        <button disabled={isLoading}>Sign Up</button>
+            <motion.span className="material-symbols-outlined"
+            onClick= {togglePasswordVisibility}
+            whileHover={{scale: 1.1}}>
+            {passwordVisible ? "visibility" : "visibility_off"}
+            </motion.span>
+            </div>
+            <label>Confirm Password:</label>
+            <div className="password-input">
+            <input 
+            type={passwordVisible ? "text" : "password"}
+            onChange={handlePasswordConfirmChange}
+            value={passwordConfirm}
+            />
+            <motion.span className="material-symbols-outlined hidden"
+            onClick= {togglePasswordVisibility}
+            whileHover={{scale: 1.1}}>
+            {passwordVisible ? "visibility" : "visibility_off"}
+            </motion.span>
+            </div>
+            {!passwordsMatch && <div className="error">Passwords do not match!</div>}
+        <button disabled={isLoading || !passwordsMatch}>Sign Up</button>
         <div id="signup-text">Already have an account?</div>
         <Link id="signup-button" to="/login">Log In</Link>
         {error && <div className = "error">{error}</div>}
         </form>
     )
 }
-export default Signup;
+export default Signup
